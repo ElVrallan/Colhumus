9fd0ca25e35b0830f169d47f039f288423019a92
ElVrallan <killbmgt@hotmail.com>
2025-04-28 10:18:39 -0500

    comentar redirecciona a noticia

--- Views/dashboard.php
+++ Views/dashboard.php
@@ -54,7 +54,7 @@
                   </svg>
                 </button>
 
-                <button class="accion-button" type="button">
+                <button class="accion-button" type="submit">
                   Comentar
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                        class="bi bi-chat-dots" viewBox="0 0 16 16">
