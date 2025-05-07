beadc95eaa5c3d0ba2b59d6c721ab67e494c8bbd
ElVrallan <killbmgt@hotmail.com>
2025-05-07 13:38:19 -0500

    clase activa navBar funcional

--- Views/includes/navbar.php
+++ Views/includes/navbar.php
@@ -1,3 +1,6 @@
+<?php
+$currentAction = $action ?? 'dashboard';
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -29,7 +32,7 @@
             <div class="nav-items">
                 <form action="index.php" method="GET" class="nav-form">
                     <input type="hidden" name="action" value="dashboard">
-                    <div class="nav-item active">
+                    <div class="nav-item <?= $currentAction === 'dashboard' ? 'active' : '' ?>">
                         <span>Inicio</span>
                         <button type="submit"></button>
                     </div>
@@ -43,7 +46,7 @@
 
                 <form action="index.php" method="GET" class="nav-form">
                     <input type="hidden" name="action" value="acercaDe">
-                    <div class="nav-item">
+                    <div class="nav-item <?= $currentAction === 'acercaDe' ? 'active' : '' ?>">
                         <span>¿Acerca de?</span>
                         <button type="submit"></button>
                     </div>
@@ -55,7 +58,7 @@
                     </svg>
                 </div>
 
-                <a class="nav-item contacto" href="https://web.whatsapp.com/send/?phone=573155829805&text&type=phone_number&app_absent=0" target="_blank">
+                <a class="nav-item contacto <?= $currentAction === 'contacto' ? 'active' : '' ?>" href="https://web.whatsapp.com/send/?phone=573155829805&text&type=phone_number&app_absent=0" target="_blank">
                     <span class="icon-contacto">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="28" fill="currentColor"
                             viewBox="0 -2 16 16">
@@ -72,24 +75,22 @@
                     </svg>
                 </div>
 
-                <div class="nav-item iniciar-sesion">
-
-                        <span class="icon-iniciar">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="28" fill="currentColor" class="bi bi-person-walking" viewBox="0 -2 16 16">
-                                <path d="M9.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M6.44 3.752A.75.75 0 0 1 7 3.5h1.445c.742 0 1.32.643 1.243 1.38l-.43 4.083a1.8 1.8 0 0 1-.088.395l-.318.906.213.242a.8.8 0 0 1 .114.175l2 4.25a.75.75 0 1 1-1.357.638l-1.956-4.154-1.68-1.921A.75.75 0 0 1 6 8.96l.138-2.613-.435.489-.464 2.786a.75.75 0 1 1-1.48-.246l.5-3a.75.75 0 0 1 .18-.375l2-2.25Z" />
-                                <path d="M6.25 11.745v-1.418l1.204 1.375.261.524a.8.8 0 0 1-.12.231l-2.5 3.25a.75.75 0 1 1-1.19-.914zm4.22-4.215-.494-.494.205-1.843.006-.067 1.124 1.124h1.44a.75.75 0 0 1 0 1.5H11a.75.75 0 0 1-.531-.22Z" />
-                            </svg>
-                        </span>
-                        <form action="index.php" method="GET">
+                <div class="nav-item iniciar-sesion <?= $currentAction === 'iniciarSesion' ? 'active' : '' ?>">
+                    <span class="icon-iniciar">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="28" fill="currentColor" class="bi bi-person-walking" viewBox="0 -2 16 16">
+                            <path d="M9.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M6.44 3.752A.75.75 0 0 1 7 3.5h1.445c.742 0 1.32.643 1.243 1.38l-.43 4.083a1.8 1.8 0 0 1-.088.395l-.318.906.213.242a.8.8 0 0 1 .114.175l2 4.25a.75.75 0 1 1-1.357.638l-1.956-4.154-1.68-1.921A.75.75 0 0 1 6 8.96l.138-2.613-.435.489-.464 2.786a.75.75 0 1 1-1.48-.246l.5-3a.75.75 0 0 1 .18-.375l2-2.25Z" />
+                            <path d="M6.25 11.745v-1.418l1.204 1.375.261.524a.8.8 0 0 1-.12.231l-2.5 3.25a.75.75 0 1 1-1.19-.914zm4.22-4.215-.494-.494.205-1.843.006-.067 1.124 1.124h1.44a.75.75 0 0 1 0 1.5H11a.75.75 0 0 1-.531-.22Z" />
+                        </svg>
+                    </span>
+                    <form action="index.php" method="GET">
                         <input type="hidden" name="action" value="iniciarSesion">
                         <div class="iniciar-text">
                             <span>Iniciar</span>
                             <span>Sesión</span>
                         </div>
                         <button type="submit"></button>
-                        </form>
+                    </form>
                 </div>
-                
 
                 <div class="divider">
                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="40" viewBox="0 0 10 40">
